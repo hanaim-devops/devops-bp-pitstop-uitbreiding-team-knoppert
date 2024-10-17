d3931d52cd25a70477516071225b7be2bb686699
Jorian Roelofsen <jorianroelofsen@gmail.com>
2024-10-17 10:56:52 +0200

    werkt nog steeds niet

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,18 +1,7 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Keycloak.AuthServices.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+﻿using Keycloak.AuthServices.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// add cors 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", builder => builder
-        .AllowAnyMethod()
-        .AllowAnyHeader()
-        .WithOrigins("http://localhost:8080", "https://localhost:8080")
-        .AllowCredentials());
-});
 
 // Set up logging
 builder.Host.UseSerilog((context, logContext) => 
@@ -20,6 +9,10 @@ builder.Host.UseSerilog((context, logContext) =>
         .ReadFrom.Configuration(builder.Configuration)
         .Enrich.WithMachineName()
 );
+
+builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+
+builder.Services.AddAuthorization();
 
 // Add framework services
 builder.Services
@@ -34,55 +27,6 @@ builder.Services.AddHttpClient<ICustomerManagementAPI, CustomerManagementAPI>();
 builder.Services.AddHttpClient<IVehicleManagementAPI, VehicleManagementAPI>();
 builder.Services.AddHttpClient<IWorkshopManagementAPI, WorkshopManagementAPI>();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = "oidc";
-})
-.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-{
-    options.Authority = "http://localhost:8080/realms/pitstop-realm";
-    options.Audience = "pitstop_client";
-    options.RequireHttpsMetadata = false;
-    options.Events = new JwtBearerEvents
-    {
-        OnAuthenticationFailed = context =>
-        {
-            Console.WriteLine($"JWT authentication failed: {context.Exception.Message}");
-            return Task.CompletedTask;
-        }
-    };
-})
-.AddOpenIdConnect("oidc", options =>
-{
-    options.Authority = "http://localhost:8080/realms/pitstop-realm";
-    options.ClientId = "pitstop_client";
-    options.RequireHttpsMetadata = false;
-    options.SaveTokens = true;
-    options.ResponseType = "code";
-    options.Scope.Add("openid");
-    options.Scope.Add("profile");
-    options.Scope.Add("email");
-    options.GetClaimsFromUserInfoEndpoint = true;
-
-    options.Events = new OpenIdConnectEvents
-    {
-        OnRemoteFailure = context =>
-        {
-            // Log detailed error
-            Console.WriteLine($"OIDC Error: {context.Failure.Message}");
-            context.HandleResponse();
-            return Task.CompletedTask;
-        },
-        OnTokenResponseReceived = context =>
-        {
-            Console.WriteLine("Token Response Received.");
-            return Task.CompletedTask;
-        }
-    };
-});
-
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -95,9 +39,6 @@ else
     app.UseHsts();
     app.UseExceptionHandler("/Home/Error");
 }
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseMvc();
 app.UseStaticFiles();
