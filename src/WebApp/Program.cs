7c5da49a9bf993391fade93e6dbca58bbee84a8f
Jorian Roelofsen <jorianroelofsen@gmail.com>
2024-10-17 09:11:31 +0200

    not working

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,9 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 
-//builder.UseKestrel();
-builder.Host.UseContentRoot(Directory.GetCurrentDirectory());
+var builder = WebApplication.CreateBuilder(args);
 
-// setup logging
+// Set up logging
 builder.Host.UseSerilog((context, logContext) => 
     logContext
         .ReadFrom.Configuration(builder.Configuration)
@@ -18,10 +21,54 @@ builder.Services
 // Add health checks
 builder.Services.AddHealthChecks();
 
-// add custom services
+// Add custom services
 builder.Services.AddHttpClient<ICustomerManagementAPI, CustomerManagementAPI>();
 builder.Services.AddHttpClient<IVehicleManagementAPI, VehicleManagementAPI>();
 builder.Services.AddHttpClient<IWorkshopManagementAPI, WorkshopManagementAPI>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(cookie =>
+{
+    cookie.Cookie.Name = "keycloak.cookie";
+    cookie.Cookie.MaxAge = TimeSpan.FromMinutes(60);
+    cookie.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    cookie.SlidingExpiration = true;
+})
+.AddOpenIdConnect(options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+    options.Authority = builder.Configuration.GetValue<string>("Keycloak:RealmUrl");
+    options.ClientId = builder.Configuration.GetValue<string>("Keycloak:ClientId");
+    options.ClientSecret = builder.Configuration.GetValue<string>("Keycloak:ClientSecret");
+    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    options.RequireHttpsMetadata = false;
+    options.CorrelationCookie.SameSite = SameSiteMode.Lax;
+    options.NonceCookie.SameSite = SameSiteMode.Lax;
+    options.SaveTokens = true;
+    options.Scope.Add("openid");
+    options.Scope.Add("email");
+    options.Scope.Add("phone");
+    options.Scope.Add("profile");
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = "name",
+        RoleClaimType = "https://schemas.scopic.com/roles"
+    };
+});
+
+// Add authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 var app = builder.Build();
 
@@ -36,10 +83,11 @@ else
     app.UseExceptionHandler("/Home/Error");
 }
 
-app.UseMvc();
-//app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.UseMvc();
+app.UseStaticFiles();
 
 app.UseHealthChecks("/hc");
 
