761936f624a3b04ba4f3dc3ecdd0ec29e75e0190
Dirk van Bruxvoort <dirkvbruxvoort@gmail.com>
2024-10-28 10:10:27 +0100

    Fix bug same line twice

--- src/WebApp/ViewModels/DIYManagementDetailsViewModel.cs
+++ src/WebApp/ViewModels/DIYManagementDetailsViewModel.cs
@@ -5,5 +5,4 @@ public class DIYManagementDetailsViewModel
     public DIYEvening DIYEvening { get; set; }
     public List<DIYFeedback> DIYFeedback { get; set; }
     public List<DIYRegistration> DIYRegistrations { get; set; }
-    public DIYEvening DIYEvening { get; set; }
 }
