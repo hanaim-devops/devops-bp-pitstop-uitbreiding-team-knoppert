bc8261c242c40227f79741fb5f9cfb6effd0df0d
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-29 17:46:54 +0100

    moet vgm zo, want zo heten de images lokaal

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -10,13 +10,13 @@
 #docker push pitstop/vehiclemanagementapi:1.0
 
 # Push all the tagged images to the same repo with different tags
-docker push jelmer0314/pitstop-team-knoppert/customermanagementapi:1.0
-docker push jelmer0314/pitstop-team-knoppert/webapp:1.0
-docker push jelmer0314/pitstop-team-knoppert/workshopmanagementeventhandler:1.0
-docker push jelmer0314/pitstop-team-knoppert/timeservice:1.0
-docker push jelmer0314/pitstop-team-knoppert/notificationservice:1.0
-docker push jelmer0314/pitstop-team-knoppert/invoiceservice:1.0
-docker push jelmer0314/pitstop-team-knoppert/auditlogservice:1.0
-docker push jelmer0314/pitstop-team-knoppert/workshopmanagementapi:1.0
-docker push jelmer0314/pitstop-team-knoppert/vehiclemanagementapi:1.0
-docker push jelmer0314/pitstop-team-knoppert/diymanagementapi:1.0
+docker push jelmer0314/pitstop/customermanagementapi:1.0
+docker push jelmer0314/pitstop/webapp:1.0
+docker push jelmer0314/pitstop/workshopmanagementeventhandler:1.0
+docker push jelmer0314/pitstop/timeservice:1.0
+docker push jelmer0314/pitstop/notificationservice:1.0
+docker push jelmer0314/pitstop/invoiceservice:1.0
+docker push jelmer0314/pitstop/auditlogservice:1.0
+docker push jelmer0314/pitstop/workshopmanagementapi:1.0
+docker push jelmer0314/pitstop/vehiclemanagementapi:1.0
+docker push jelmer0314/pitstop/diymanagementapi:1.0
