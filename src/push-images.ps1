f4101c170046170c82d26c9e83b511a375c9a107
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-29 19:25:22 +0100

    ik weet het echt niet meer

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -1,3 +1,3 @@
-docker tag pitstop/customermanagementapi:1.0 jelmer0314/pitstop/customermanagementapi:1.0
+docker tag pitstop/customermanagementapi:1.0 jelmer0314/customermanagementapi:1.0
 
-docker push jelmer0314/pitstop/customermanagementapi:1.0
+docker push jelmer0314/customermanagementapi:1.0
