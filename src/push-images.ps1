20e2f5da2a5066d1a09f3ebd5d72a6f69a6eddb7
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-29 17:30:39 +0100

    dit werkt als het goed is!

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -1,13 +1,13 @@
-docker push pitstop/customermanagementapi:1.0
-docker push pitstop/customermanagementapi:2.0
-docker push pitstop/webapp:1.0
-docker push pitstop/workshopmanagementeventhandler:1.0
-docker push pitstop/timeservice:1.0
-docker push pitstop/notificationservice:1.0
-docker push pitstop/invoiceservice:1.0
-docker push pitstop/auditlogservice:1.0
-docker push pitstop/workshopmanagementapi:1.0
-docker push pitstop/vehiclemanagementapi:1.0
+#docker push pitstop/customermanagementapi:1.0
+#docker push pitstop/customermanagementapi:2.0
+#docker push pitstop/webapp:1.0
+#docker push pitstop/workshopmanagementeventhandler:1.0
+#docker push pitstop/timeservice:1.0
+#docker push pitstop/notificationservice:1.0
+#docker push pitstop/invoiceservice:1.0
+#docker push pitstop/auditlogservice:1.0
+#docker push pitstop/workshopmanagementapi:1.0
+#docker push pitstop/vehiclemanagementapi:1.0
 
 # Push all the tagged images to the same repo with different tags
 docker push jelmer0314/pitstop-team-knoppert/customermanagementapi:1.0
