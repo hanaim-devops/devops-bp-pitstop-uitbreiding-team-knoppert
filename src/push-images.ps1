41fcb9c917616c9fd5b7c04d1d13438d9566291a
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-29 19:36:46 +0100

    bruh wat wou ik kan helemaal niet in de public docker regristry

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -1,3 +1,36 @@
-docker tag pitstop/customermanagementapi:1.0 jelmer0314/customermanagementapi:1.0
+# Set your Docker Hub username and image tag
+$DOCKER_USERNAME = 'jelmer0314'
+$IMAGE_TAG = '1.0'
 
-docker push jelmer0314/customermanagementapi:1.0
+# List of services
+$services = @(
+    'customermanagementapi',
+    'webapp',
+    'workshopmanagementeventhandler',
+    'timeservice',
+    'notificationservice',
+    'invoiceservice',
+    'auditlogservice',
+    'workshopmanagementapi',
+    'vehiclemanagementapi',
+    'diymanagementapi'
+)
+
+# Retag and push images for each service
+foreach ($service in $services) {
+    # Original image name
+    $originalImage = "pitstop/${service}:${IMAGE_TAG}"
+    
+    # New image name with Docker Hub username and simplified naming
+    $newImage = "${DOCKER_USERNAME}/pitstop-${service}:${IMAGE_TAG}"
+    
+    # Retag the image
+    Write-Host "Retagging $originalImage to $newImage"
+    docker tag $originalImage $newImage
+    
+    # Push the image to Docker Hub
+    Write-Host "Pushing $newImage to Docker Hub"
+    docker push $newImage
+}
+
+Write-Host "All images have been retagged and pushed successfully."
