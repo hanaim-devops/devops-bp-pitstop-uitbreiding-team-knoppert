9f63c42099d3714a31f33c8008be12fd6f2fb466
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-29 15:39:27 +0100

    init minikube test

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -8,3 +8,15 @@ docker push pitstop/invoiceservice:1.0
 docker push pitstop/auditlogservice:1.0
 docker push pitstop/workshopmanagementapi:1.0
 docker push pitstop/vehiclemanagementapi:1.0
+
+# Push all the tagged images to the same repo with different tags
+docker push jelmer0314/pitstop-team-knoppert/customermanagementapi:1.0
+docker push jelmer0314/pitstop-team-knoppert/webapp:1.0
+docker push jelmer0314/pitstop-team-knoppert/workshopmanagementeventhandler:1.0
+docker push jelmer0314/pitstop-team-knoppert/timeservice:1.0
+docker push jelmer0314/pitstop-team-knoppert/notificationservice:1.0
+docker push jelmer0314/pitstop-team-knoppert/invoiceservice:1.0
+docker push jelmer0314/pitstop-team-knoppert/auditlogservice:1.0
+docker push jelmer0314/pitstop-team-knoppert/workshopmanagementapi:1.0
+docker push jelmer0314/pitstop-team-knoppert/vehiclemanagementapi:1.0
+docker push jelmer0314/pitstop-team-knoppert/diymanagementapi:1.0
