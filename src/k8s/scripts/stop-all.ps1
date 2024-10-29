24871239bbed3b366defd484ceedc9d7c4296109
Jelmer Noppert <j.noppert@student.han.nl>
2024-10-29 20:06:26 +0100

    error omdat bij sotp de delete niet werkt

--- src/k8s/scripts/stop-all.ps1
+++ src/k8s/scripts/stop-all.ps1
@@ -4,10 +4,4 @@ kubectl delete deploy --all -n pitstop
 kubectl delete virtualservice --all -n pitstop
 kubectl delete destinationrule --all -n pitstop
 
-kubectl delete svc --all -n monitoring
-kubectl delete deploy --all -n monitoring
-kubectl delete configmap --all -n monitoring
-kubectl delete pvc --all -n monitoring
-kubectl delete pv --all -n monitoring
 
-kubectl delete namespace monitoring
