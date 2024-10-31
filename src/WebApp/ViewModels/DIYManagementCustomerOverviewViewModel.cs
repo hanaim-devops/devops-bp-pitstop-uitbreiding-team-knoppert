c646634b6b5fa3e2ecf8a2d149a3856e9f2d6d8f
JorianRoelofsen <93914476+JorianRoelofsen@users.noreply.github.com>
2024-10-31 11:31:52 +0100

    45 als klant wil ik in het overzicht de details van de eerstvolgende doe het zelf avond (#80)
    * tussentijds
    
    * not working
    
    * still not working
    
    * working
    
    * added frontend and filtering
    
    * review
    
    * Update src/WebApp/Controllers/DIYManagementController.cs
    
    Co-authored-by: knoppert <94072994+pietknoppert@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: knoppert <94072994+pietknoppert@users.noreply.github.com>

---
+++ src/WebApp/ViewModels/DIYManagementCustomerOverviewViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pitstop.WebApp.ViewModels
+{
+    public class DIYManagementCustomerOverviewViewModel
+    {
+        public IEnumerable<DIYEvening> DIYEvening { get; set; }
+
+        public List<DIYRegistration> DIYRegistrations { get; set; }
+    }
+}
